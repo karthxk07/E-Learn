c8cc6d78af598e73e6f98234f7a015f5a072cf49
Karthik <109301536+karthxk07@users.noreply.github.com>
2023-01-01 21:28:01 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,28 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+
+function redirect(){
+	window.location = "home.html";
+}
+
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Test" && password == "Test#123"){
+	alert("Login Successfully!");
+	window.location = "success.html"; // Redirecting to other page.
+
+	return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
